c7e3a76d671b2b635dab1ae418c194b614f2715a
Lee-Hyeongkyeong <dlgudrud116@gmail.com>
2025-04-07 11:28:51 +0900

    feat: all complete 기능 추가

--- week5/app.js
+++ week5/app.js
@@ -1,14 +1,23 @@
 import TodoController from "./controller/TodoController.js";
+import CompleteController from "./controller/CompleteController.js";
 
 
 const addBtn = document.getElementById('input-button');
 const input = document.querySelector('input');
-//const allComBtn = document.getElementById('all-com-button');
+const allComBtn = document.getElementById('all-com-button');
+const todoList = document.getElementById('to-do-list');
+
 
 addBtn.addEventListener('click', () => {
     const todoController = new TodoController(input.value);
     todoController.addTodo();
 })
 
-//allComBtn.addEventListener('click', ()=> {
-//})
+allComBtn.addEventListener('click', ()=> {
+    const todoThings = Array.from(todoList.children).filter(child => !child.classList.contains('title'));
+    todoThings.forEach(thing => {
+        let allComplete = new CompleteController(thing.textContent);
+        allComplete.addToComplete();
+        todoList.removeChild(thing);
+    });
+})
